03357323b4d4ee5de9e5075c01c6a080b6d71e97
JeronimoHoulin <jeronimo.houlin@gmail.com>
2022-04-11 16:18:58 -0300

    FIREBASE integration

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCRk65ziWg9hhMu7vho4JpHMe1pga1eoIA",
+    authDomain: "jerotesteo1.firebaseapp.com",
+    databaseURL: "https://jerotesteo1-default-rtdb.firebaseio.com",
+    projectId: "jerotesteo1",
+    storageBucket: "jerotesteo1.appspot.com",
+    messagingSenderId: "285230177474",
+    appId: "1:285230177474:web:b52f871e8692145f49ef5b",
+    measurementId: "G-T8BDRVR7RQ"
+  };
