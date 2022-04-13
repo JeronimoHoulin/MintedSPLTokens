99de4f2f0e70a50e2e186b39a833e0e203e6082e
JeronimoHoulin <jeronimo.houlin@gmail.com>
2022-04-12 21:58:52 -0300

    FINAL button

--- src/components/ByVertical/game/game_tokens.js
+++ src/components/ByVertical/game/game_tokens.js
@@ -26,6 +26,34 @@ function GameFetch (){
 
     const [show, setShow] = useState(false)
     const [gamedict, setGamedict] = useState([]) 
+    const [checked, setChecked] = useState([]);
+
+// Add/Remove checked item from list
+const handleCheck = (event) => {
+    var updatedList = [...checked];
+    if (event.target.checked) {
+      updatedList = [...checked, event.target.value];
+    } else {
+      updatedList.splice(checked.indexOf(event.target.value), 1);
+    }
+    setChecked(updatedList);
+  };
+
+  // Generate string of checked items
+  const checkedItems = checked.length
+    ? checked.reduce((total, item) => {
+        return total + ", " + item;
+      })
+    : "";
+
+  // Return classes based on whether item is checked
+  var isChecked = (item) =>
+    checked.includes(item) ? "checked-item" : "not-checked-item";
+
+
+
+
+
 
     let game_tokens = [];
     let adrs_arr = []
@@ -259,20 +287,24 @@ function GameFetch (){
                             <th>Viewed</th>
                         </tr>
 
-                        {gamedict.map(function(item, i){
-                            return(parse(
-                                `<tr>
-                                    <td><img src="${item.logo}" width="34" height="35"/></td>
-                                    <td>${item.Symbol}</td>
-                                    <td>${item.Name}</td>
-                                    <td>${item.Address}</td>
-                                    <td>${item.Tags}</td>
-                                    <td>${item.Extensions}</td>
-                                    <td>${convertUnixTime(item.Timestamp)}</td>
-                                </tr>`)
-                            )
-                        })}
-                                                
+
+
+                        {gamedict.map((item) => (
+                            <tr>
+                                <img src={`${item.logo}`} width="34" height="35"></img>
+                                <td>{item.Symbol}</td>
+                                <td>{item.Name}</td>
+                                <td>{item.Address}</td>
+                                <td>{item.Tags}</td>
+                                <input value={item.Address} type="checkbox" onChange={handleCheck} />
+                                <span className={isChecked(item.Address)}>{item.Symbol}</span>
+                            </tr>
+                        ))}
+
+
+                        
+
+
                     </table>
                     
                 </div>
